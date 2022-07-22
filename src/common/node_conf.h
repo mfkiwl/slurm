e22d4da49a72ef60f7c16cc01446896814d8e52d
Scott Hilton <scott@schedmd.com>
2022-07-22 12:09:55 -0600

    Use the actual number of nodes in the system
    With the dynamic nodes changes, node_record_count no longer reflects the
    actual number of nodes in the system.
    
    Here I replaced it in locations where it seems the number of active
    nodes was sought instead of the usable size of the node_record_table.
    
    Bug 14231

--- src/common/node_conf.h
+++ src/common/node_conf.h
@@ -468,3 +468,10 @@ extern bitstr_t *node_conf_get_active_bitmap(void);
 extern void node_conf_set_all_active_bits(bitstr_t *b);
 
 #endif /* !_HAVE_NODE_CONF_H */
+
+/*
+ * Count all active nodes.
+ *
+ * node_record_table_ptr may have NULL slots in it, so only count non-null nodes
+ */
+extern int node_conf_get_active_node_count(void);
